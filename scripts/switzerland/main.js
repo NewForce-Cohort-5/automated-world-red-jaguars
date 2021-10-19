bba758021b6f0ae57f7aec0d8becceb237a3b72d
cbgriffith <c.b.griffith@outlook.com>
2021-10-19 16:00:40 -0400

    last save for the day

--- scripts/switzerland/main.js
+++ scripts/switzerland/main.js
@@ -1,5 +1,7 @@
 import { renderNav } from "../ui/renderNav.js"
 import { SwissCityList } from "./cities/CityList.js"
+import { SwissLandmarkList } from "./landmarks/LandmarkList.js"
 
 renderNav()
 SwissCityList()
+SwissLandmarkList()
