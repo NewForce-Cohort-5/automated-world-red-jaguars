9b2d4e8a3763c00c6bc06b4ab0aeffdb03efca4c
Andrew Birt <abirt1986@gmail.com>
2021-10-19 15:56:08 -0400

    finished adding country data and have functions to make components

---
+++ scripts/netherlands/main.js
@@ -0,0 +1,9 @@
+import { renderNav } from "../ui/renderNav.js";
+import { CityList } from "./cities/CityList.js";
+import { LandmarkList } from "./landmarks/LandmarkList.js";
+import { CelebrityList } from "./celebrities/CelebrityList.js";
+
+renderNav();
+CityList();
+LandmarkList();
+CelebrityList();
