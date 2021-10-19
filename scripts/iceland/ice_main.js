3d2c29779a55f84f8c64acde072931cda74cef0d
bdigregs <bmdigregorio@gmail.com>
2021-10-19 15:35:35 -0400

    third commit

--- scripts/iceland/ice_main.js
+++ scripts/iceland/ice_main.js
@@ -1,5 +1,7 @@
 import { renderNav } from "../ui/renderNav.js";
 import { CitiesList } from "./cities/CitiesList.js";
+import {LandmarkList} from "./landmarks/LandmarkList.js";
 
 renderNav()
 CitiesList()
+LandmarkList()
