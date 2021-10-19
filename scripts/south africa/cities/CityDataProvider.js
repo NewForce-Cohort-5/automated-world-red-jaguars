1e4db1abed51ad24f6345ed5cd3548c38232f007
Jaytwy24 <jtwy24@gmail.com>
2021-10-19 10:56:47 -0400

    south africa city data completed

--- scripts/south africa/cities/CityDataProvider.js
+++ scripts/south africa/cities/CityDataProvider.js
@@ -1,25 +1,29 @@
-const cityCollection = [
+const africanCityCollection = [
     {
         name: "Johannesburg",
-        description: "Johannesburg, renowned for its thunderstorms, is ironically the Economic powerhouse of Africa! 'Joburg' as it is called by locals, was founded in the heydays of one of the World's biggest and richest gold rushes - earning it the name of 'Egoli' meaning 'place of gold'.",
-        link: "https://sahistory.org.za/place/johannesburg-gauteng-province",
+        description: "Renowned for its thunderstorms, it is ironically the Economic powerhouse of Africa! 'Joburg' as it is called by locals, was founded in the heydays of one of the World's biggest and richest gold rushes - earning it the name of 'Egoli' meaning 'place of gold'.",
+        link: "Click for more info",
         landmark:"Constitution Hill",
-        population:5926668
+        population:"5,926,668"
     },
 
     {
         name: "Rustenburg",
         description: "The Magaliesberg range of Mountains dominates the Landscape that is now known as the Northwest Province. Rustenburg, the largest City in this Province, has significance for many groups in South Africa, and its History is complex and rich in variety.",
-        link: "https://www.sahistory.org.za/place/rustenburg",
+        link: "Click for more info",
         landmark:"Pilanesberg National Park",
-        population:549575
+        population:"549,575"
     },
 
     {
         name: "Cape Town",
         description: "Cape Town is one of South Africa’s most Historically important Cities. It was here, in the Mother City, where the first European colonists set foot in South Africa—which also marked the beginning of the South African slave trade.",
-        link: "https://www.sahistory.org.za/place/cape-town-western-province",
+        link: "Click for more info",
         landmark:"Table Mountain",
-        population:4709990
+        population:"4,709,990"
     },
 ]
+
+export const useAfricaCity = () => {
+    return africanCityCollection.slice()
+}
---
+++ scripts/south africa/cities/CityList.js
@@ -0,0 +1,29 @@
+import { useAfricaCity } from "./CityDataProvider.js"
+import { AfricaCity } from "./City.js"
+
+
+
+
+export const AfricaCityList = () => {
+
+
+
+    const contentElement = document.querySelector(".africa-city-list")
+    const africaCities = useAfricaCity()
+
+    let africaCityHTMLRepresentations = ""
+    for (const singleAfricaCityObject of africaCities) {
+
+
+
+       africaCityHTMLRepresentations += AfricaCity(singleAfricaCityObject)
+    }
+
+    
+
+    contentElement.innerHTML += `
+        <article class="africa-city-list">
+        ${africaCityHTMLRepresentations}  
+        </article>
+    `
+}
