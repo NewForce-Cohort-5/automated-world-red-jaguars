496d1d0645464a9f1d2719c685c060cf26f5881f
Jaytwy24 <jtwy24@gmail.com>
2021-10-20 09:44:14 -0400

    celebrity upates

--- scripts/ui/Celebrity.js
+++ scripts/ui/Celebrity.js
@@ -1,6 +1,6 @@
 export const Celeb = (celeb) => {
     return `
-         <section class="city-card">
+         <section class="celeb-card">
             <div class="celeb-name"><a class="celeb-link" href="${celeb.link}"><b>${celeb.name}</a></b></div>
             <div class="celeb-desc"><i>${celeb.description}</i></div>
         </section>
