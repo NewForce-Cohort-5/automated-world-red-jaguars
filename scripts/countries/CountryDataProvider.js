ee457e1ba441aad18701a0306e3302ed19214aa9
Jaytwy24 <jtwy24@gmail.com>
2021-10-20 15:07:48 -0400

    style sheet updates

---
+++ scripts/countries/CountryDataProvider.js
@@ -0,0 +1,33 @@
+const CountryCollection = [
+    {
+        image:"/images/table-mountain.png",
+        name: "Iceland",
+        description: "Known as the most iconic landmark of South Africa, an national park, once named as one of the World's 7 wonders. It's famous cable car has taken millions of people to it's peak.",
+        link: "https://tablemountainnationalpark.org/"
+    },
+
+    {
+        image: "images/Old-Fort-sunset.jpg",
+        name: "Netherlands",
+        description: "The site of incarceration in South Africa that imprisoned a large number of world renowned men and women such as Nelson Mandela and Mahatma Gandhi",
+        link: "https://www.constitutionhill.org.za/"
+    },
+
+    {
+        image: "images/p-nationalpark.jpg",
+        name: "South Africa",
+        description: "Among Southern Africa’s fascinating game reserves, Pilanesberg National Park is possibly the most accessible. Situated in the ecologically rich transition zone between the Kalahari and the Lowveld, this vast area promises thrilling big game viewing in a malaria free environment. ",
+        link: "https://thecommonwealth.org/our-member-countries/south-africa"
+    },
+
+    {
+        image: "images/p-nationalpark.jpg",
+        name: "South Africa",
+        description: "Among Southern Africa’s fascinating game reserves, Pilanesberg National Park is possibly the most accessible. Situated in the ecologically rich transition zone between the Kalahari and the Lowveld, this vast area promises thrilling big game viewing in a malaria free environment. ",
+        link: "https://thecommonwealth.org/our-member-countries/south-africa"
+    },
+]
+
+export const useAfricaLandmark = () => {
+    return africanLandmarkCollection.slice()
+}
