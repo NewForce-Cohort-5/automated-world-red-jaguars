70789ff1f4732aeb9528abadaf48c56bb25682e6
bdigregs <bmdigregorio@gmail.com>
2021-10-21 11:05:02 -0400

    render footer and set active

--- scripts/iceland/ice_main.js
+++ scripts/iceland/ice_main.js
@@ -2,8 +2,12 @@ import { renderNav } from "../ui/renderNav.js";
 import { CitiesList } from "./cities/CitiesList.js";
 import {LandmarkList} from "./landmarks/LandmarkList.js";
 import { CelebsList} from "./celebrities/CelebritiesList.js"
+import { setActive } from "../helper/setActive.js";
+import { renderFooter } from "../ui/renderFooter.js"
 
 renderNav()
 CitiesList()
 LandmarkList()
 CelebsList()
+setActive()
+renderFooter()
