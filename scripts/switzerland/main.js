2394e0fa36904fd64ada442dc11e0056f4512db1
cbgriffith <c.b.griffith@outlook.com>
2021-10-21 10:41:22 -0400

    added footer

--- scripts/switzerland/main.js
+++ scripts/switzerland/main.js
@@ -3,9 +3,11 @@ import { setActive } from "../helper/setActive.js";
 import { SwissCityList } from "./cities/CityList.js"
 import { SwissLandmarkList } from "./landmarks/LandmarkList.js"
 import { SwissCelebrityList } from "./celebrities/CelebrityList.js"
+import { renderFooter } from "../ui/renderFooter.js"
 
 renderNav()
 setActive()
 SwissCityList()
 SwissLandmarkList()
 SwissCelebrityList()
+renderFooter()
---
+++ scripts/ui/Footer.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return `
+         <section class="footer">
+            <footer>Team Red Jaguars &copy 2021</footer>
+        </section>
+    `
+}
